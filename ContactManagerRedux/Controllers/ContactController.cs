33a823593aea6dd1c7247539a4c27686e803756a
Robert Bercovic <rbercovic@gmail.com>
2017-09-11 23:19:53 -0400

    Initial Push

---
+++ ContactManagerRedux/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using ContactManagerRedux.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ContactManagerRedux.Services;
+
+namespace ContactManagerRedux.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
